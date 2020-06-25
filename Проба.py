99fc690ad466b4a64d1b6e252313d966acb552b6
gbrs <61385511+gbrs@users.noreply.github.com>
2020-06-25 11:07:38 +0300

    tmt100

--- Проба.py
+++ Проба.py
@@ -1,7 +1,8 @@
 value = 1
 count = 2
-d = list((value))
+d = list('a')
 print(d)
+
 
 '''
 В темплейты lgg
@@ -12,4 +13,21 @@ logger.disabled = False
 logging.debug(f'')
 
 '''
+
+"""
+В темплейты tmt100
+import timeit
+
+MySetup = '''
+$NOT_REPITED_BLOCK$
+'''
+
+MyCode = ''' 
+$TESTED_CODE$
+'''
+
+# вывод на печать результатов измерений number = 100 повторов
+print(timeit.repeat(stmt=MyCode, setup=MySetup, number=100))
+
+"""
 
