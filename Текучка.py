ba0d44cfdce9b950a349632e278d6a7dc22c0dfa
gbrs <61385511+gbrs@users.noreply.github.com>
2020-07-21 09:56:00 +0300

    решил 3.6.2

--- Текучка.py
+++ Текучка.py
@@ -1,23 +1,33 @@
 """
-Скачайте файл. В нём указан адрес другого файла, который нужно скачать
-с использованием модуля requests и посчитать число строк в нём.
+Имеется набор файлов, каждый из которых, кроме последнего,
+содержит имя следующего файла.
+Первое слово в тексте последнего файла: "We".
 
-Используйте функцию get для получения файла
-(имеет смысл вызвать метод strip к передаваемому параметру, чтобы убрать пробельные символы по краям).
+Скачайте предложенный файл. В нём содержится ссылка на первый файл из этого набора.
 
-После получения файла вы можете проверить результат, обратившись к полю text.
-Если результат работы скрипта не принимается, проверьте поле url на правильность.
-Для подсчёта количества строк разбейте текст с помощью метода splitlines.
+Все файлы располагаются в каталоге по адресу:
+https://stepic.org/media/attachments/course67/3.6.3/
 
-В поле ответа введите одно число или отправьте файл, содержащий одно число.
+Загрузите содержимое ﻿последнего файла из набора, как ответ на это задание.
 """
 
 import requests
 
-with open('dataset_3378_2.txt') as f:
-    url = f.read().strip()
-print(url)
 
-file = requests.get(url)
-print(file)
-print(len(file.text.splitlines()))
+def get_file(file_url):
+    """Печатает файл, начинающийся с 'We', или рекурентно вызывает себя"""
+    # print('file_url: ', file_url)
+    file = requests.get(file_url)
+    if file.text[:2] == 'We':
+        print(file.text)
+    else:
+        # next_file_name = file.text.strip()
+        # print('next_file_url:', next_file_url)
+        get_file('https://stepic.org/media/attachments/course67/3.6.3/' +
+                 file.text.strip())
+    pass
+
+with open('dataset_3378_3.txt') as f:
+    init_file_url = f.read().strip()
+    # print('init_file_name: ', init_file_url)
+    get_file(init_file_url)
